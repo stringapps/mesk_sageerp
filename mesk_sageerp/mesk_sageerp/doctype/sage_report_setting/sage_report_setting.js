3ab307026e285ab4a6fb1f236178986b90507072
D-codE <d-code@D-codEs-MacBook-Pro.local>
2026-03-26 16:28:26 +0530

    feat:SFSR Report and setting files

---
+++ mesk_sageerp/mesk_sageerp/doctype/sage_report_setting/sage_report_setting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, D-codE and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Sage Report Setting", {
+// 	refresh(frm) {
+
+// 	},
+// });
