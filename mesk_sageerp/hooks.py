543e9966dfc0dcea82d37fa28e897ffc30aeabed
D-codE <d-code@D-codEs-MacBook-Pro.local>
2026-04-01 10:56:18 +0530

    feat: Add Sage General Ledger report with filters and data retrieval
    - Implemented JavaScript file for report filters and UI elements.
    - Created JSON configuration for the Sage General Ledger report.
    - Developed Python script to handle data fetching, validation, and processing for the report.
    - Included various filters such as company, finance book, date range, account selection, and party details.
    - Added functionality to categorize entries by voucher, account, or party.
    - Integrated accounting dimensions and options for displaying values in different currencies.

--- mesk_sageerp/hooks.py
+++ mesk_sageerp/hooks.py
@@ -240,3 +240,18 @@ scheduler_events = {
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+fixtures =[
+    {
+        "doctype": "Custom Field",
+        "filters": [
+            [
+                "name",
+                "in",
+                ["Supplier-custom_vendor_id",
+					]
+			
+			]
+        ]
+                
+	}
+]
---
+++ mesk_sageerp/mesk_sageerp/report/sage_general_ledger/sage_general_ledger.js
@@ -0,0 +1,256 @@
+// Copyright (c) 2026, D-codE and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Sage General Ledger"] = {
+	filters: [
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			reqd: 1,
+		},
+		{
+			fieldname: "finance_book",
+			label: __("Finance Book"),
+			fieldtype: "Link",
+			options: "Finance Book",
+			read_only: 1,
+		},
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			reqd: 1,
+			width: "60px",
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1,
+			width: "60px",
+		},
+		{
+			fieldname: "account",
+			label: __("Account"),
+			fieldtype: "MultiSelectList",
+			options: "Account",
+			read_only: 1,
+			hidden: 1,
+			get_data: function (txt) {
+				return frappe.db.get_link_options("Account", txt, {
+					company: frappe.query_report.get_filter_value("company"),
+				});
+			},
+			read_only: 1,
+		},
+		{
+			fieldname: "voucher_no",
+			label: __("Voucher No"),
+			fieldtype: "Data",
+			read_only: 1,
+			hidden: 1,
+			on_change: function () {
+				frappe.query_report.set_filter_value("categorize_by", "Categorize by Voucher (Consolidated)");
+			},
+		},
+		{
+			fieldname: "against_voucher_no",
+			label: __("Against Voucher No"),
+			fieldtype: "Data",
+			read_only: 1,
+			hidden: 1,
+		},
+		{
+			fieldtype: "Break",
+		},
+		{
+			fieldname: "party_type",
+			label: __("Party Type"),
+			fieldtype: "Autocomplete",
+			read_only: 1,
+			hidden: 1,
+			options: Object.keys(frappe.boot.party_account_types),
+			on_change: function () {
+				frappe.query_report.set_filter_value("party", []);
+			},
+		},
+		{
+			fieldname: "party",
+			label: __("Party"),
+			fieldtype: "MultiSelectList",
+			options: "party_type",
+			hidden: 1,
+			get_data: function (txt) {
+				if (!frappe.query_report.filters) return;
+
+				let party_type = frappe.query_report.get_filter_value("party_type");
+				if (!party_type) return;
+
+				return frappe.db.get_link_options(party_type, txt);
+			},
+			on_change: function () {
+				var party_type = frappe.query_report.get_filter_value("party_type");
+				var parties = frappe.query_report.get_filter_value("party");
+
+				if (!party_type || parties.length === 0 || parties.length > 1) {
+					frappe.query_report.set_filter_value("party_name", "");
+					frappe.query_report.set_filter_value("tax_id", "");
+					return;
+				} else {
+					var party = parties[0];
+					var fieldname = erpnext.utils.get_party_name(party_type) || "name";
+					frappe.db.get_value(party_type, party, fieldname, function (value) {
+						frappe.query_report.set_filter_value("party_name", value[fieldname]);
+					});
+
+					if (party_type === "Customer" || party_type === "Supplier") {
+						frappe.db.get_value(party_type, party, "tax_id", function (value) {
+							frappe.query_report.set_filter_value("tax_id", value["tax_id"]);
+						});
+					}
+				}
+			},
+		},
+		{
+			fieldname: "party_name",
+			label: __("Party Name"),
+			fieldtype: "Data",
+			hidden: 1,
+			
+		},
+		{
+			fieldname: "categorize_by",
+			label: __("Categorize by"),
+			fieldtype: "Select",
+			options: [
+				"",
+				{
+					label: __("Categorize by Voucher"),
+					value: "Categorize by Voucher",
+				},
+				{
+					label: __("Categorize by Voucher (Consolidated)"),
+					value: "Categorize by Voucher (Consolidated)",
+				},
+				{
+					label: __("Categorize by Account"),
+					value: "Categorize by Account",
+				},
+				{
+					label: __("Categorize by Party"),
+					value: "Categorize by Party",
+				},
+			],
+			hidden: 1,
+			default: "Categorize by Voucher (Consolidated)",
+		},
+		{
+			fieldname: "tax_id",
+			label: __("Tax Id"),
+			fieldtype: "Data",
+			hidden: 1,
+		},
+		{
+			fieldname: "presentation_currency",
+			label: __("Currency"),
+			fieldtype: "Select",
+			options: erpnext.get_presentation_currency_list(),
+			hidden: 1,
+		},
+		{
+			fieldname: "cost_center",
+			label: __("Cost Center"),
+			fieldtype: "MultiSelectList",
+			options: "Cost Center",
+			hidden: 1,
+			get_data: function (txt) {
+				return frappe.db.get_link_options("Cost Center", txt, {
+					company: frappe.query_report.get_filter_value("company"),
+				});
+			},
+		},
+		{
+			fieldname: "project",
+			label: __("Project"),
+			fieldtype: "MultiSelectList",
+			options: "Project",
+			hidden: 1,
+			get_data: function (txt) {
+				return frappe.db.get_link_options("Project", txt, {
+					company: frappe.query_report.get_filter_value("company"),
+				});
+			},
+		},
+		{
+			fieldname: "include_dimensions",
+			label: __("Consider Accounting Dimensions"),
+			fieldtype: "Check",
+			default: 1,
+			hidden: 1,
+		},
+		{
+			fieldname: "show_opening_entries",
+			label: __("Show Opening Entries"),
+			fieldtype: "Check",
+			hidden: 1,
+		},
+		{
+			fieldname: "include_default_book_entries",
+			label: __("Include Default FB Entries"),
+			fieldtype: "Check",
+			default: 1,
+			hidden: 1,
+		},
+		{
+			fieldname: "show_cancelled_entries",
+			label: __("Show Cancelled Entries"),
+			fieldtype: "Check",
+			hidden: 1,
+		},
+		{
+			fieldname: "show_net_values_in_party_account",
+			label: __("Show Net Values in Party Account"),
+			fieldtype: "Check",
+			hidden: 1,
+		},
+		{
+			fieldname: "show_amount_in_company_currency",
+			label: __("Show Credit / Debit in Company Currency"),
+			fieldtype: "Check",
+			hidden: 1,
+		},
+		{
+			fieldname: "add_values_in_transaction_currency",
+			label: __("Add Columns in Transaction Currency"),
+			fieldtype: "Check",
+			hidden: 1,
+		},
+		{
+			fieldname: "show_remarks",
+			label: __("Show Remarks"),
+			fieldtype: "Check",
+			hidden: 1,
+		},
+		{
+			fieldname: "ignore_err",
+			label: __("Ignore Exchange Rate Revaluation and Gain / Loss Journals"),
+			fieldtype: "Check",
+			hidden: 1,
+		},
+		{
+			fieldname: "ignore_cr_dr_notes",
+			label: __("Ignore System Generated Credit / Debit Notes"),
+			fieldtype: "Check",
+			hidden: 1,
+		},
+	],
+	collapsible_filters: true,
+	seperate_check_filters: true,
+};
+
+erpnext.utils.add_dimensions("Sage General Ledger", 15);
